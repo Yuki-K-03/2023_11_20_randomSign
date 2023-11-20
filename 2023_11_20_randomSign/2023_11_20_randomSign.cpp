08056a9884ceec525cb67ef48d0ff22f48f5794e
Kawashima_Y <146790933+Yuki-K-03@users.noreply.github.com>
2023-11-20 20:28:02 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_11_20_randomSign/2023_11_20_randomSign.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main()
+{
+    srand((int)time(NULL));
+
+    for (int i = 0; i < 100; i++) {
+        printf("%2d ", (rand() % 2 * 2 - 1));
+    }
+
+    return 0;
+}
